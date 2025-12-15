61d5fcd620f18940c282b47cc02c77f07da6f7c1
Sagar <sadduri@adobe.com>
2025-12-15 19:09:10 +0530

    Enhance cards component with custom styles and image integration

--- blocks/form/components/cards/cards.js
+++ blocks/form/components/cards/cards.js
@@ -3,20 +3,29 @@
  * Based on: Radio Group
  */
 
+import { createOptimizedPicture } from '../../../../scripts/aem.js';
+
 /**
  * Decorates a custom form field component
- * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper. Refer to the documentation for its structure for each component.
+ * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper.
+ * Refer to the documentation for its structure for each component.
  * @param {Object} fieldJson - The form json object for the component.
  * @param {HTMLElement} parentElement - The parent container element of the field.
  * @param {string} formId - The unique identifier of the form.
  */
 export default async function decorate(fieldDiv, fieldJson, parentElement, formId) {
   console.log('⚙️ Decorating cards component:', fieldDiv, fieldJson, parentElement, formId);
-  
+
   // TODO: Implement your custom component logic here
   // You can access the field properties via fieldJson.properties
   // You can access the parent container via parentElement
   // You can access the form ID via formId
-  
+
+  fieldDiv.classList.add('card');
+  fieldDiv.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
+    const image = createOptimizedPicture('https://main--afb--jalagari.hlx.live/lab/images/card.png', 'card-image');
+    radioWrapper.appendChild(image);
+  });
+
   return fieldDiv;
 }
